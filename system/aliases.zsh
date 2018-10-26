5bee28b53060319da712146909e5766692afa54e
Marcin Gajewski <code@marcingajew.ski>
2018-10-26 20:51:15 +0200

    Couple of aliases

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,2 +1,8 @@
 # weather
 wttr() { curl wttr.in/"${1:-krakow}"; }
+
+alias dotfiles='cd ~/.dotfiles'
+alias zshrc='vim ~/.dotfiles/zsh/zshrc.symlink'
+alias vimrc='vim ~/.dotfiles/vim/vimrc.symlink'
+
+
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,6 +2,7 @@ alias reload!='. ~/.zshrc'
 alias old_gcc='export CXX=/usr/bin/g++-5.4'
 
 alias cls='clear'
+alias help='tldr'
 alias serve='python -m http.server'
 unsetopt auto_pushd
 unsetopt pushd_ignore_dups
@@ -9,9 +10,14 @@ unsetopt pushd_ignore_dups
 alias l='ls++'
 alias cat='bat'
 alias ping='./prettyping --nolegend'
-alias preview="fzf --preview 'bat --color \"always\" {}'"
 alias du="ncdu --color dark -rr -x --exclude .git --exclude node_modules"
 
 # Not add to history
 alias t=' trans'
 alias ag=' ag'
+
+# FZF
+alias preview="fzf --preview 'bat --color \"always\" --line-range 0:30 {}'"
+alias fvim='vim $(preview)'
+alias fta='tmux attach -t $(tl | fzf | tr ":" "\n" | head -n1)'
+alias ftk='tmux kill-session -t $(tl | fzf | tr ":" "\n" | head -n1)'
