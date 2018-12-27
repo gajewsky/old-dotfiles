043715fbb47c1f01782f51a1bd557913785d3507
Marcin Gajewski <code@gajewsky.it>
2018-12-27 20:05:04 +0100

    Ag is too slow

--- fzf/env.zsh
+++ fzf/env.zsh
@@ -1,4 +1,4 @@
 export FZF_DEFAULT_OPTS="--bind='ctrl-o:execute(nvim {})+abort'"
-export FZF_DEFAULT_COMMAND='ag -g ""'
+export FZF_DEFAULT_COMMAND='rg --files --no-ignore-vcs --hidden'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_DEFAULT_OPS="--extended"
