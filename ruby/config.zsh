e8c7e66d46ec0baa84be177e8457bf40f56ab212
Marcin Gajewski <code@gajewsky.it>
2018-12-27 19:55:08 +0100

    More vim snippets

--- ruby/config.zsh
+++ ruby/config.zsh
@@ -1,5 +1,5 @@
-source /usr/local/share/chruby/chruby.sh
-source /usr/local/share/chruby/auto.sh
+source /usr/share/chruby/chruby.sh
+source /usr/share/chruby/auto.sh
 
 chruby_default_gems=~/.chruby-default-gems/chruby-default-gems.sh
 [ -f $chruby_default_gems ] && source $chruby_default_gems
