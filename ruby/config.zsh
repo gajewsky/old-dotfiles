fe97f477f11a15cec0d03b23141e565f463d37f9
Marcin Gajewski <code@gajewsky.it>
2018-12-27 18:56:15 +0100

    Update ruby setup

--- ruby/config.zsh
+++ ruby/config.zsh
@@ -1,3 +1,7 @@
-source /usr/share/chruby/chruby.sh
-source /usr/share/chruby/auto.sh
+source /usr/local/share/chruby/chruby.sh
+source /usr/local/share/chruby/auto.sh
+
+chruby_default_gems=~/.chruby-default-gems/chruby-default-gems.sh
+[ -f $chruby_default_gems ] && source $chruby_default_gems
+
 
