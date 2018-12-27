fb8d83d7a75a784b583ad71211263014f6f845e5
Marcin Gajewski <code@gajewsky.it>
2018-12-27 18:49:01 +0100

    Update node setup

--- node/aliases.zsh
+++ node/aliases.zsh
@@ -1,1 +1,3 @@
 alias unfuck_js="rm -rf tmp/ node_modules/ bower_components/ dist/ && bower cache clean && npm cache clean --force && npm install && bower install"
+alias ember="./node_modules/.bin/ember"
+
