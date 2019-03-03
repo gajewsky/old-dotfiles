15a435bc1be321a2ea9d1fdbddf4f5172b852358
Marcin Gajewski <code@gajewsky.it>
2019-03-03 21:14:58 +0100

    Vim improvements

--- ruby/config.zsh
+++ ruby/config.zsh
@@ -1,7 +1,2 @@
-source /usr/share/chruby/chruby.sh
-source /usr/share/chruby/auto.sh
-
-chruby_default_gems=~/.chruby-default-gems/chruby-default-gems.sh
-[ -f $chruby_default_gems ] && source $chruby_default_gems
 
 
